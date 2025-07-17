770676ce7f6a508d3f8f793e1e09eb13292df8c9
it-gluesolutions <it.gluesolutions.gr@gmail.com>
2025-07-17 18:39:35 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -4,12 +4,27 @@ const STATIC_ASSETS = [
   "./index.html",
   "./styles.css",
   "./app.js",
-  "./calc.js"
+  "./calc.js",
+  "./manifest.webmanifest"
+  // Add icons here if you want them cached offline:
+  // "./icons/icon-192.png",
+  // "./icons/icon-512.png"
 ];
 
 self.addEventListener("install", e => {
   e.waitUntil(
     caches.open(CACHE_NAME).then(cache => cache.addAll(STATIC_ASSETS))
+  );
+});
+
+self.addEventListener("activate", e => {
+  // Optional: cleanup old caches
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.map(k => (k !== CACHE_NAME ? caches.delete(k) : undefined))
+      )
+    )
   );
 });
 
