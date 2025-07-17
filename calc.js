6f0b76f162c4fbf89edea0f88c4c3ffc0b18420c
it-gluesolutions <it.gluesolutions.gr@gmail.com>
2025-07-17 16:53:53 +0300

    Update calc.js

--- calc.js
+++ calc.js
@@ -1,5 +1,14 @@
 export function get_glue_mass(p1, p2, p3, p4, h1, h2, h3) {
   // TODO: Implement your actual calculation logic here.
-  // For now, return a dummy sum as placeholder:
+  // For now, return a dummy sum as placeholder:\
+  /*
+    p1: Ταχύτητα μηχανής
+    p2: μήκος πάνελ
+    p3: ύψος πάνελ
+    p4: Μήκος διάκενου
+    h1: Ρυθμόσς τήξης
+    h2: Απαιτούμενα γραμ. κόλλας ανά τ.μ.
+    h3: δεν χρησιμοπ.
+  */
   return p1 + p2 + p3 + p4 + h1 + h2 + h3;
 }
