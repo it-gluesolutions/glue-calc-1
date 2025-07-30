457f8bc53385a5d99e3faec4a4ff3f3f2c62b5c9
it-gluesolutions <it.gluesolutions.gr@gmail.com>
2025-07-30 21:45:09 +0300

    Update app.js

--- app.js
+++ app.js
@@ -150,10 +150,10 @@ navigator.serviceWorker.addEventListener('message', (event) => {
     if (event.data.info){
       cache_update.info = event.data.info;
     } else {
-      if ('ERROR' == cache_update.result)
-        cache_update.urls.error.push(cache_update.url);
+      if ('ERROR' == event.data.result)
+        cache_update.urls.error.push(event.data.url);
       else
-        cache_update.urls.ok.push(cache_update.url);
+        cache_update.urls.ok.push(event.data.url);
       if (cache_update.info == cache_update.urls.ok.length + cache_update.urls.error.length){
         alert(JSON.stringify(cache_update.urls));
       }
