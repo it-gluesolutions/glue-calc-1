e0f2713553ef2e4d4e12b4821f524cd77dd70f78
it-gluesolutions <it.gluesolutions.gr@gmail.com>
2025-08-27 00:52:04 +0300

    Update datalists.js

--- datalists.js
+++ datalists.js
@@ -1,6 +1,6 @@
 {
   "surface_density": {
-    "Feelt PU (607.40): 140,
+    "Feelt PU (607.40)": 140,
     "Unfeelt PU (608.00)": 100,
     "Feelt EVA (280.50)": 230,
     "Unfeelt EVA (280.30)": 200
